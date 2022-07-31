037fd420dc98c465687ed8708a0e5cd4de1c07f0
dnitra <109307365+dnitra@users.noreply.github.com>
2022-07-31 21:22:47 +0200

    finished exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    
+    if (string==="") return string
+    
+    return reverseString(string.slice(1)) + string.slice(0,1)
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
